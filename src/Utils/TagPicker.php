324426b7e59ad7687d26adbedc2719a7f37588f1
xypp <2952795729@qq.com>
2024-07-12 10:46:45 +0800

    与之前的错误拼写兼容

--- src/Utils/TagPicker.php
+++ src/Utils/TagPicker.php
@@ -1,105 +1,118 @@
 <?php
 namespace Xypp\PayToRead\Utils;
-class TagPicker{
-    public static function TagPicker(string $content,bool $updateId = true){
+
+class TagPicker
+{
+    public static function TagPicker(string $content, bool $updateId = true)
+    {
         $inCodeBlock = false;
         $queue = array();
         $closedTag = array();
         $startPos = array();
         $result = "";
-        for($i=0;$i<strlen($content);$i++){
-            if(!$inCodeBlock && substr($content,$i,4) == "[pay"){
+        for ($i = 0; $i < strlen($content); $i++) {
+            if (!$inCodeBlock && substr($content, $i, 4) == "[pay") {
                 $start = $i;
-                $end = strpos($content,"]",$i);
-                if($end === false){
+                $end = strpos($content, "]", $i);
+                if ($end === false) {
                     break;
                 }
-                $p = substr($content,$i+1,$end-$i-1);
-                $parts = explode(" ",$p);
+                $p = substr($content, $i + 1, $end - $i - 1);
+                $parts = explode(" ", $p);
                 $params = array();
-                foreach($parts as $p){
-                    $args = explode("=",$p);
-                    if(count($args)!=2)continue;
-                    $arg = trim($args[0],"\"'` \t\r\n");
-                    $val = trim($args[1],"\"'` \t\r\n");
+                foreach ($parts as $p) {
+                    $args = explode("=", $p);
+                    if (count($args) != 2)
+                        continue;
+                    $arg = trim($args[0], "\"'` \t\r\n");
+                    $val = trim($args[1], "\"'` \t\r\n");
+                    //Fix typo
+                    if ($arg == "ammount")
+                        $arg = "amount";
                     $params[$arg] = $val;
                 }
-                array_push($queue,array("start_tag"=>array($start,$end),"end_tag"=>null,"params"=>$params));
+                array_push($queue, array("start_tag" => array($start, $end), "end_tag" => null, "params" => $params));
                 $i = $end;
-            }else if(!$inCodeBlock && substr($content,$i,6) == "[/pay]"){
-                if(!count($queue))continue;
+            } else if (!$inCodeBlock && substr($content, $i, 6) == "[/pay]") {
+                if (!count($queue))
+                    continue;
                 $top = array_pop($queue);
-                $top['end_tag'] = array($i,$i+5);
-                $top['params']['depth']=count($queue);
-                $top['params']['new']=false;
-                $idx = array_push($closedTag,$top) - 1;
+                $top['end_tag'] = array($i, $i + 5);
+                $top['params']['depth'] = count($queue);
+                $top['params']['new'] = false;
+                $idx = array_push($closedTag, $top) - 1;
                 $startPos[$idx] = $top['start_tag'][0];
                 $i = $i + 5;
-            }else if(substr($content,$i,3) == '```'){
+            } else if (substr($content, $i, 3) == '```') {
                 $i = $i + 2;
                 $inCodeBlock = !$inCodeBlock;
             }
         }
-        if($updateId){
+        if ($updateId) {
             $idmx = 0;
             arsort($startPos);
-            foreach($startPos as $i=>$_){
+            foreach ($startPos as $i => $_) {
                 $tg = $closedTag[$i];
-                if(!isset($tg['params']['id'])){
+                if (!isset($tg['params']['id'])) {
                     $id = $idmx++;
-                    $closedTag[$i]['params']['new']=true;
-                    $closedTag[$i]['params']['id']=$id;
-                    $content = substr_replace($content," id=[newId]#".$id."#[/newId]",$tg['start_tag'][1],0);
+                    $closedTag[$i]['params']['new'] = true;
+                    $closedTag[$i]['params']['id'] = $id;
+                    $content = substr_replace($content, " id=[newId]#" . $id . "#[/newId]", $tg['start_tag'][1], 0);
                 }
             }
         }
-        return [$closedTag,$content];
+        return [$closedTag, $content];
     }
-    public static function TagPickerHTML(string $content){
+    public static function TagPickerHTML(string $content)
+    {
         $inCodeBlock = false;
         $queue = array();
         $closedTag = array();
-        for($i=0;$i<strlen($content);$i++){
-            if(!$inCodeBlock && substr($content,$i,24) == "<div class=\"pay-to-read\""){
+        for ($i = 0; $i < strlen($content); $i++) {
+            if (!$inCodeBlock && substr($content, $i, 24) == "<div class=\"pay-to-read\"") {
                 $start = $i;
-                $end = strpos($content,">",$i);
-                if($end === false){
+                $end = strpos($content, ">", $i);
+                if ($end === false) {
                     break;
                 }
-                $p = substr($content,$i+1,$end-$i-1);
-                $parts = explode(" ",$p);
+                $p = substr($content, $i + 1, $end - $i - 1);
+                $parts = explode(" ", $p);
                 $params = array();
-                foreach($parts as $p){
-                    $args = explode("=",$p);
-                    if(count($args)!=2)continue;
-                    $arg = trim($args[0],"\"'` \t\r\n\0\x0B");
-                    if(substr($arg,0,5)=="data-"){
-                        $arg = substr($arg,5);
+                foreach ($parts as $p) {
+                    $args = explode("=", $p);
+                    if (count($args) != 2)
+                        continue;
+                    $arg = trim($args[0], "\"'` \t\r\n\0\x0B");
+                    if (substr($arg, 0, 5) == "data-") {
+                        $arg = substr($arg, 5);
                     }
-                    $val = trim($args[1],"\"'` \t\r\n\0\x0B");
+                    //Fix typo
+                    if ($arg == "ammount")
+                        $arg = "amount";
+                    $val = trim($args[1], "\"'` \t\r\n\0\x0B");
                     $params[$arg] = $val;
                 }
-                array_push($queue,array("start_tag"=>array($start,$end),"end_tag"=>null,"params"=>$params));
+                array_push($queue, array("start_tag" => array($start, $end), "end_tag" => null, "params" => $params));
                 $i = $end;
-            }else if(!$inCodeBlock && substr($content,$i,33) == "<pay-to-read></pay-to-read></div>"){
+            } else if (!$inCodeBlock && substr($content, $i, 33) == "<pay-to-read></pay-to-read></div>") {
                 $top = array_pop($queue);
-                $top['end_tag'] = array($i,$i+32);
-                $top['params']['depth']=count($queue);
-                $top['params']['new']=false;
-                array_push($closedTag,$top);
+                $top['end_tag'] = array($i, $i + 32);
+                $top['params']['depth'] = count($queue);
+                $top['params']['new'] = false;
+                array_push($closedTag, $top);
                 $i = $i + 30;
-            }else if(substr($content,$i,4) == '<pre'){
-                $cnxt = substr($content,$i+4,1);
-                if($cnxt == ">" || $cnxt == " " || $cnxt == "\t"){
+            } else if (substr($content, $i, 4) == '<pre') {
+                $cnxt = substr($content, $i + 4, 1);
+                if ($cnxt == ">" || $cnxt == " " || $cnxt == "\t") {
                     $i = $i + 5;
                     $inCodeBlock = true;
                 }
-            }else if(substr($content,$i,6) == '</pre>'){
+            } else if (substr($content, $i, 6) == '</pre>') {
                 $i = $i + 5;
                 $inCodeBlock = false;
             }
         }
-        return [$closedTag,$content];
+        return [$closedTag, $content];
     }
 
 }
