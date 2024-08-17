25690276aa63dacfb49bfdffc4f4b39ab7c0058f
xypp <2952795729@qq.com>
2024-08-17 13:19:24 +0800

    添加了条件判断

--- src/Console/MigrateData.php
+++ src/Console/MigrateData.php
@@ -27,21 +27,19 @@ class MigrateData extends Command
         $this->withProgressBar(Post::where("type", "comment")->get()->getIterator(), function (CommentPost $post) use (&$haveNotMigrate) {
             $content = $post->getAttribute("content");
             if ($post->getAttribute("content_with_payitem")) {
-                $haveNotMigrate = true;
-                $content = $post->getAttribute("content_with_payitem");
+                $this->error("You have not run the command `php flarum migrate`. Please run it first.");
+                throw new \Exception("You have not run the command `php flarum migrate`. Please run it first.");
             }
             if (str_starts_with($content, "!old!")) {
                 $content = substr($content, 5);
                 $post->setContentAttribute($content);
-            }
-            if ($haveNotMigrate) {
-                $post->setAttribute("content_with_payitem", null);
+                if ($haveNotMigrate) {
+                    $post->setAttribute("content_with_payitem", null);
+                }
             }
             $post->save();
         });
-        if ($haveNotMigrate) {
-            $this->warn("You have not run the command `php flarum migrate`. Please run it.");
-        }
+
         $this->info("Migration completed.");
     }
 }
